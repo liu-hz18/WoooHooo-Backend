58789a3a1df02b812ad01f57ead2282b21922c79
unknown <zhang-ht18@mails.tsinghua.edu.cn>
2020-10-21 23:18:56 +0800

    models

--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+class User(models.Model):
+    #用户名
+    name = models.CharField(unique=True, max_length=200)
+    #密码
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
