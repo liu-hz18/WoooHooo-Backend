855d423fc7b3066118df021dab6c2dda22ace0d8
liu-hz18 <1456437967@qq.com>
2020-10-22 11:18:49 +0800

    feat: provide search and news type api to frontend
    fix: solve pylint error in api
    
    change: add jieba in requirements
    
    fix: add total return in search get api
    
    fix: bad response will return <total>
    
    fix: bad response will return <total>
    
    upd: change hardcoded url to API.js
    
    update:change the reponse information if something is wrong
    
    fix:change the way to get the user information from Post request
    
    fix:change the way to get the user information from Post request
    
    fix:change the way to get the user information from Post request
    
    update:print the request body after receive the request
    
    fix: update the request
    
    fix:change the GET request in login
    
    fix:change the way to get request items
    
    fix:change the way to get request items
    
    fix:change the way to get request items
    
    fix:change the way to get request items
    
    change: move useless / url and update GET api
    
    upd: keyword cut will be sorted by length
    
    upd: keyword cut will be sorted by length
    
    upd: add 	otal param in news type api
    
    upd: provide keywords field in news type return
    
    fix:change the password length limit
    
    add: news type fetch api from newsdb
    
    fix: pylint error in nesapi.py
    
    update:add the code that enable the Django-backend to connect to the java-backend
    
    update:add the code that enable the Django-backend to connect to the java-backend
    
    update:add the code that enable the Django-backend to connect to the java-backend
    
    fix: use requests to fetch lucene data
    
    solve confict: merge dev into master
    
    fix: finish backend request to lucene #3 #4

--- app/views.py
+++ app/views.py
@@ -1,8 +1,83 @@
 """ app/views.py """
+import json
+import jieba
+from django.http.response import JsonResponse
 from django.shortcuts import HttpResponse
+
+from .newsapi import fetch_typed_news, fetch_search_result
 # Create your views here.
 
 
 def index(request):
     """function index"""
     return HttpResponse("Hello world")
+
+def search(request):
+    """
+    backend search api to frontend
+    request api: 
+        post:
+            {
+                "newstype": news type,
+                "page": page offset,
+                "number": return news number
+            }
+            return {
+                data: newslist
+            }
+        get:
+           {
+               "query": query string,
+               "page": page offset,
+               "number": return number news
+           } 
+           return {
+               data: newslist,
+               total: total result,
+               keywords: use `jieba` to split query
+           }
+    """
+    def gen_bad_response(code: int, data: list, keywords: list):
+        return JsonResponse({
+            'code': code,
+            'data': data,
+            'keywords': keywords,
+            'total': 0,
+        }, status=code)
+    if request.method == "GET":
+        keywords = []
+        page = int(request.GET.get('page', default=0))
+        number = int(request.GET.get('number', default=10))
+        query = request.GET.get('query', default="")
+        keywords = sorted(jieba.lcut_for_search(query), key=len, reverse=True)
+        print(page, number, query, keywords)
+        if page < 0 or number > 100 or query == "":       
+            return gen_bad_response(400, [], keywords)
+        try:
+            total, newslist = fetch_search_result(query, number, page)
+        except Exception as e:
+            total, newslist = 0, []
+            print("error in search():", e)
+        return JsonResponse({
+            'code': 200,
+            'data': newslist,
+            'keywords': keywords,
+            'total': total
+        }, status=200)
+    elif request.method == "POST":
+        print(request.body.decode())
+        json_obj = json.loads(request.body.decode())
+        page = json_obj.get('page')
+        number = json_obj.get('number')
+        news_type = json_obj.get('newstype')
+        print(page, number, news_type)
+        if page is None or number is None or news_type is None or page < 0 or number > 100:
+            return gen_bad_response(400, [], [news_type])
+        page, number = int(page), int(number)
+        total, newslist = fetch_typed_news(news_type, number, page)
+        return JsonResponse({
+            'code': 200,
+            'data': newslist,
+            'keywords': [],
+            'total': total,
+        }, status=200)
