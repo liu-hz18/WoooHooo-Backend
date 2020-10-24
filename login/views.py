e095165acc5623948de11008e2971c4f5726bc77
张瀚天 <zhang-ht18@mails.tsinghua.edu.cn>
2020-10-24 21:51:02 +0800

    fix:change the GET request in login

--- login/views.py
+++ login/views.py
@@ -17,17 +17,10 @@ def gen_response(code: int, data: str):
 def login(request):
     #Get用来验证登录
     if request.method == 'GET':
-<<<<<<< HEAD
         print(request.GET.items())
         username = request.GET.get('username', default="")
         password = request.GET.get('userpass', default="")
         print("username: ", username, "password: ", password)
-=======
-        user = json.loads(request.body.decode())
-        print(user)
-        username = user.get('username')
-        password = user.get('userpass')
->>>>>>> f9d1a1b (fix: update the request)
         #如果前端没传过来用户名和密码
         if password == "" or username == "":
             return gen_response(405, "there is no username or password or None")
@@ -42,16 +35,11 @@ def login(request):
         return gen_response(401, "password Error")
     #用Post来完成注册
     elif request.method == 'POST':
-<<<<<<< HEAD
         print(request.body.decode())
         try:
             user = json.loads(request.body.decode())
         except json.JSONDecodeError:
             return gen_response(403 , "the data is not json")
-=======
-        user = json.loads(request.body.decode())
-        print(user)
->>>>>>> 4b9561b (update:print the request body after receive the request)
         username = user.get('username')
         password = user.get('userpass')
         if not username or not password:
@@ -63,15 +51,8 @@ def login(request):
         user = User(name=username,password=password)
         print(user)
         try:
-<<<<<<< HEAD
             user.full_clean() #检查用户名的有效性
             user.save() # 存入数据库
-=======
-            #检查用户名的有效性
-            user.full_clean()
-            # 存入数据库
-            user.save()
->>>>>>> f9d1a1b (fix: update the request)
             return gen_response(200, "user was set successfully")
         except ValidationError as e:
             return gen_response(400, "length Error of user: {}".format(e))
