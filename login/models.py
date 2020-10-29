3cb581fe764be3c3ecf77fa5d5867dd4964496fb
张瀚天 <zhang-ht18@mails.tsinghua.edu.cn>
2020-10-29 14:49:05 +0800

    fix:change the password length limit

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     #用户名
     name = models.CharField(unique=True, max_length=200)
     #密码
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
